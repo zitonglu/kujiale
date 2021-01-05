4dea89152695b4ca597d9d14cbb39fb9fd8f48a8
TBM <910109610@qq.com>
2021-01-05 17:23:18 +0800

    no end

--- index.php
+++ index.php
@@ -59,16 +59,54 @@ $sign = array(
 );
 $md5 = md5($sign['appSecret'].$sign['appkey'].$sign['date']);
 
-// 创建CURL变量并输出相关 2020-1-5
-$url = 'https://sandbox-openapi.kujiale.com/v2/root/node?';
-$url .= 'timestamp='.$sign['date'].'&';
-$url .= 'appkey='.$sign['appkey'].'&';
-$url .= 'sign='.md5($sign['appSecret'].$sign['appkey'].$sign['date']);
+$addArray = array(
+    'appkey' => $sign['appkey'],
+    'timestamp' => $timestamp,
+    'sign' => $md5
+);
+
+// 获取账号体系根节点信息 
+//open.kujiale.com/open/apps/1/docs?doc_id=434&tab_id=api&path=0_58_433_434&tag_id=3
+// $url = 'https://sandbox-openapi.kujiale.com/v2/root/node';
+// $url .= '?timestamp='.$sign['date'].'&';
+// $url .= 'appkey='.$sign['appkey'].'&';
+// $url .= 'sign='.md5($sign['appSecret'].$sign['appkey'].$sign['date']);
+// echo '<b>URL:</b> '.$url.'<br>';
+// echo '<b>timestamp:</b> '.$timestamp.'<br>';
+// echo '<b>sign:</b> '.md5($sign['appSecret'].$sign['appkey'].$sign['date']).'<br>';
+// $echo = geturl($url);
+// echo '<b>获取账号体系根节点信息:</b><br><pre> ';
+// var_dump($echo);
+// echo '</pre><br>';
+
+//账号注册绑定接口
+//open.kujiale.com/open/apps/1/docs?doc_id=524&tab_id=api&path=0_58_433_524&tag_id=3
+$url = 'https://sandbox-openapi.kujiale.com/v2/register';
+// $url .= '?timestamp='.$sign['date'].'&';
+// $url .= 'appkey='.$sign['appkey'].'&';
+// $url .= 'appuid=1&';
+// $url .= 'sign='.md5($sign['appSecret'].$sign['appkey'].'1'.$sign['date']);
 echo '<b>URL:</b> '.$url.'<br>';
-echo '<b>timestamp:</b> '.$timestamp.'<br>';
-echo '<b>sign:</b> '.md5($sign['appSecret'].$sign['appkey'].$sign['date']).'<br>';
-$echo = geturl($url);
-echo '<b>return:</b><br><pre> ';
-var_dump($echo);
-echo '</pre><br>';
+$data = array('name'=>'ABC');
+//$data = array_merge($data,$addArray);
+$headerArray = array('Content-Type:application/json;charset=utf-8;',',"Accept:application/json"');
+// $echo = posturl($url,$data);
+// echo '<b>建部门节点:</b><br><pre> ';
+// var_dump($echo);
+// echo '</pre><br>';
+
+$data  = json_encode($data);    
+        $headerArray =array("Content-type:application/json;charset='utf-8'","Accept:application/json");
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST,FALSE);
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl,CURLOPT_HTTPHEADER,$headerArray);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($curl);
+        curl_close($curl);
+        echo json_decode($output，true);
+
 ?>
